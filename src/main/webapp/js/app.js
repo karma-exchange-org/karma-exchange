4a84ce9698747a2e6e465b7e6a7f2ef3ea2af498
Harish Balijepalli <fake.email@gmail.com>
2013-05-08 23:09:46 -0700

    Adding basic event html templete

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,5 +1,5 @@
 
-var kexApp = angular.module("kexApp", ["ngResource","ngCookies"]).
+var kexApp = angular.module("kexApp", ["ngResource","ngCookies","google-maps"]).
     config(function($routeProvider,$httpProvider) {
         $routeProvider.
             when('/', { controller: homeCtrl, templateUrl: 'partials/home.html' }).
@@ -24,6 +24,27 @@ kexApp.factory('Events', function($resource) {
     return $resource('/api/event/:id', { id: '@id' }, { update: { method: 'POST' } });
 });
 
+kexApp.directive('googleplace', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, model) {
+            var options = {
+                types: [],
+                componentRestrictions: {}
+            };
+            scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+            google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+                scope.$apply(function() {
+                    model.$setViewValue(element.val());                
+                });
+            });
+        }
+    };
+});
+var geocoder = new google.maps.Geocoder();
+        
+
 
 
 var eventsCtrl = function ($scope, $location, $http, Events,$cookieStore) {
@@ -45,9 +66,30 @@ var eventsCtrl = function ($scope, $location, $http, Events,$cookieStore) {
 
 };
 
-var addEventsCtrl = function($scope, $location, Events) {
+var addEventsCtrl = function($scope, $location, Events, geocoder) {
 
 	checkLogin($location);
+
+    angular.extend($scope, {
+
+        /** the initial center of the map */
+        center: {
+            latitude: 37,
+            longitude: -122
+        },
+
+        /** the initial zoom level of the map */
+        zoom: 4,
+
+        /** list of markers to put in the map */
+        markers: [ {
+                
+            }],
+
+        // These 2 properties will be set when clicking on the map
+        clicked: null,  
+        clicked: null,
+    });
     $scope.save = function () {
     	Events.save($scope.item, function() {
             $location.path('/events');
@@ -56,14 +98,89 @@ var addEventsCtrl = function($scope, $location, Events) {
 };
 
 var editEventsCtrl =  function ($scope, $routeParams, $location, Events) {
-	$scope.item = Events.get({ id: $routeParams.itemId });
+
+    angular.extend($scope, {
+
+        /** the initial center of the map */
+        center: {
+            latitude: 37,
+            longitude: -122
+        },
+
+        /** the initial zoom level of the map */
+        zoom: 4,
+
+        /** list of markers to put in the map */
+        markers: [ {}],
+
+        // These 2 properties will be set when clicking on the map
+        clicked: null,  
+        clicked: null,
+        
+    });
+
+    $scope.refreshMap = function(){
+                geocoder.geocode({ 'address': $scope.item.location.address.street+','+$scope.item.location.address.city+','+$scope.item.location.address.state+','+$scope.item.location.address.country }, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    $scope.center.latitude = results[0].geometry.location.lat();
+                    $scope.center.longitude = results[0].geometry.location.lng();
+                    $scope.addMarker($scope.center.latitude,$scope.center.longitude);
+                    $scope.zoom = 15;
+                    $scope.$apply();
+                    
+                    
+                }
+
+    })};
+
+    $scope.addMarker = function (markerLat,markerLng) {
+            $scope.markers.push({
+                latitude: parseFloat(markerLat),
+                longitude: parseFloat(markerLng)
+            });
+            
+            
+        };
+        
+    $scope.findMe = function () {
+        
+        if ($scope.geolocationAvailable) {
+            
+            navigator.geolocation.getCurrentPosition(function (position) {
+                
+                $scope.center = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                };
+                
+                $scope.$apply();
+            }, function () {
+                
+            });
+        }   
+    };            
+
+    
+
+	
 
 	$scope.save = function () {
 		Events.update({id: $scope.item.id}, $scope.item, function () {
 	        $location.path('/events');
 	    });
 	};
-	};
+
+    $scope.item = Events.get({ id: $routeParams.itemId } ,function() {
+        $scope.refreshMap();
+
+        }, function(response) {
+        //404 or bad
+        console.log(response);
+        if(response.status === 404) {
+    }});
+    $scope.findMe();
+};
+    
 
 var checkLogin = function($location){
 	if($.cookie("facebook-token"))
