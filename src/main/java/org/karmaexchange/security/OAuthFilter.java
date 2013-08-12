491468f3c5fb73f73333b08e4afabc0b7c63cef1
Amir Valiani <first.last@gmail.com>
2013-08-12 15:54:39 -0700

    Change oAuth filter logging to Level.FINE

--- src/main/java/org/karmaexchange/security/OAuthFilter.java
+++ src/main/java/org/karmaexchange/security/OAuthFilter.java
@@ -38,7 +38,7 @@ import com.googlecode.objectify.VoidWork;
 
 public class OAuthFilter implements Filter {
 
-  public static final Level OAUTH_LOG_LEVEL = Level.OFF;
+  public static final Level OAUTH_LOG_LEVEL = Level.FINE;
   private static final Logger log = Logger.getLogger(OAuthFilter.class.getName());
 
   @SuppressWarnings("unused")
